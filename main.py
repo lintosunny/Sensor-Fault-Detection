cd40fe9e297059eff5b8cd4e4bb57c8058cdf32d
lintosunny <lintosunny111@gmail.com>
2024-08-08 21:34:24 +0530

    created constant variables

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-from sensor.configuration.mongo_db_connection import MongoDBClienct
 
+from sensor.configuration.mongo_db_connection import MongoDBClienct
+from sensor.exception import SensorException
+import sys, os
+'''
 if __name__ == '__main__':
     mongodb_client = MongoDBClienct()
     print("collection name: ",mongodb_client.database.list_collection_names())
+    '''
+
+def test_exception():
+    try:
+        x=1/0
+    except Exception as e:
+        raise SensorException(e, sys)
+
+if __name__=='__main__':
+    try:
+        test_exception()
+    except Exception as e:
+        print(e)
